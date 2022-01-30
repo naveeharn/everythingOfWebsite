b2bd7814e68ebc7d8247814c00508855e9981f7b
naveeharn <naveeharn@hotmail.com>
2022-01-30 20:31:53 +0700

    first commit

---
+++ 3_JavaScript/06_datatype/06_script.js
@@ -0,0 +1,29 @@
+let money = 500
+let price=112.44
+
+let message = "JavaScript"
+let name = "naveeharn"
+
+let isCorrect = true
+
+money += 500
+console.log(money)
+console.log(price)
+console.log(isCorrect)
+
+console.log(typeof(money))
+console.log(typeof(price))
+console.log(typeof(message))
+console.log(typeof(name))
+console.log(typeof(isCorrect))
+
+let num = 112+44
+console.log(num)
+num = "112"+44
+console.log(num)
+num = 44+"112"
+console.log(num)
+num = "112"+"44"
+console.log(num)
+
+
