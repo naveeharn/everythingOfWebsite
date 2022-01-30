1c91df1e6cf8667998d198c245f4ba1088b4b95f
naveeharn <naveeharn@hotmail.com>
2022-01-30 20:31:44 +0700

    first commit

---
+++ 3_JavaScript/05_variable/05_script.js
@@ -0,0 +1,20 @@
+let firstname,lastname, Firstname
+firstname="naveeharn"
+console.log(firstname)
+lastname="tehmarn"
+console.log(lastname)
+Firstname="Naveeharn"
+console.log(Firstname)
+
+let age = 19
+console.log(age)
+age = 20
+console.log(age)
+// PI = 3.141
+
+document.writeln(firstname)
+document.writeln(lastname)
+document.writeln(age) 
+console.log("Hello",firstname,lastname,age,"years old")
+
+let _for = "_for", $for = "$for"
