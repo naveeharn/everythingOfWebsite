9b032552aaa252602e42b7618008e64532d9aae8
naveeharn <naveeharn@hotmail.com>
2022-01-31 22:47:51 +0700

    first commit

---
+++ 3_JavaScript/12_prefix-postfix/12_script.js
@@ -0,0 +1,21 @@
+let a = 5, b = 10
+
+console.log(a)
+console.log("before = ",a)
+console.log("prefix = ",++a)
+console.log("after  = ",a)
+
+console.log(a)
+console.log("before = ",a)
+console.log("postfix= ",a++)
+console.log("after  = ",a)
+
+console.log(b)
+console.log("before = ",b)
+console.log("prefix = ",--b)
+console.log("after  = ",b)
+
+console.log(b)
+console.log("before = ",b)
+console.log("postfix= ",b--)
+console.log("after  = ",b)
