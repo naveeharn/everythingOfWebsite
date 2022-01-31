e026f71a38cb8e06263f5f977003612cb3751e5b
naveeharn <naveeharn@hotmail.com>
2022-01-31 22:46:32 +0700

    first commit

---
+++ 3_JavaScript/08_array/08_script.js
@@ -0,0 +1,11 @@
+let list = [1,2,3,4,5] 
+console.log(list)
+
+list = new Array()
+console.log(list)
+
+list = new Array(1,2,3,4,5)
+console.log(list," \tFirst item : "+list[0])
+
+list = new Array(1,"2",'3',4.5,true,false)
+console.log(list)
