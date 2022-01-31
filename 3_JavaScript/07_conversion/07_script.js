89d0fcaab48e0f5f89b78bf06011b50915b32661
naveeharn <naveeharn@hotmail.com>
2022-01-31 22:46:19 +0700

    first commit

---
+++ 3_JavaScript/07_conversion/07_script.js
@@ -0,0 +1,22 @@
+var integer = "112"
+var float = "0.44"
+
+console.log(typeof(integer))
+integer = parseInt(integer)
+console.log(typeof(integer))
+
+console.log(typeof(float))
+float = parseInt(float)
+console.log(typeof(float))
+
+var age = 112
+
+console.log(typeof(age))
+age = toString(age)
+console.log(typeof(age))
+
+age = 112
+
+console.log(typeof(age))
+age = " " + age
+console.log(typeof(age))
