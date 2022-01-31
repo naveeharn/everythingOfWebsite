f142c9c8cb11c0763197235dfcfbcdd0f7df337c
naveeharn <naveeharn@hotmail.com>
2022-01-31 22:47:20 +0700

    first commit

---
+++ 3_JavaScript/10_compare/10_script.js
@@ -0,0 +1,18 @@
+console.log("1==2",1==2)
+console.log("3==3",3==3)
+console.log("1>2",1>2)
+console.log("1<2",1<2)
+console.log("1>=2",1>=2)
+console.log("1<=2",1<=2)
+
+let a = 44, b = 112
+
+let isEqual = a==b
+
+console.log(a,b,"isEqual=",isEqual)
+
+let isInequal = a!=b
+
+console.log(a,b,"isInequal=",isInequal)
+
+console.log(a,b,"!isInequal=",!isInequal)
