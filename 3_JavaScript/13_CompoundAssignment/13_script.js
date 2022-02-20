57ac6130742361958dd695611ea2c90399314386
naveeharn <naveeharn@hotmail.com>
2022-02-20 21:47:27 +0700

    first commit

---
+++ 3_JavaScript/13_CompoundAssignment/13_script.js
@@ -0,0 +1,34 @@
+let x = 1, y = 2
+
+console.log("before  = ", x)
+x = x + y
+console.log("x = x+y = ", x)
+x += y
+console.log("x += y  = ", x)
+
+console.log("before  = ", x)
+x = x - y
+console.log("x = x-y = ", x)
+x -= y
+console.log("x -= y  = ", x)
+
+console.log("before  = ", x)
+x = x * y
+console.log("x = x*y = ", x)
+x *= y
+console.log("x *= y  = ", x)
+
+console.log("before  = ", x)
+x = x / y
+console.log("x = x/y = ", x)
+x /= y
+console.log("x /= y  = ", x)
+
+x = 10
+y = 3
+
+console.log("before  = ", x)
+x = x % y
+console.log("x = x%y = ", x)
+x %= y
+console.log("x %= y  = ", x)
