c12288cd90df143ffe0f7b0520ee7ae1c12a9c71
naveeharn <naveeharn@hotmail.com>
2022-02-22 21:31:57 +0700

    first commit

---
+++ 3_JavaScript/29_function/29_script.js
@@ -0,0 +1,17 @@
+function print() {
+    console.log("Hello World")
+}
+
+function message(){
+    alert("message for you : kuy")
+}
+
+function displayName() {
+    document.write("naveeharn")
+}
+
+print()
+print()
+
+message()
+
