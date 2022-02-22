84542acbed65c5e1abe64d70f62714758b9c989f
naveeharn <naveeharn@hotmail.com>
2022-02-22 21:32:16 +0700

    first commit

---
+++ 3_JavaScript/30_parameter/30_script.js
@@ -0,0 +1,22 @@
+function plus(x) {
+    x++;
+    console.log(x)
+}
+
+function display(username) {
+    console.log("Hello " , username)
+}
+
+function fullname(firstname, lastname) {
+    console.log("Welcome " , firstname , " " , lastname)
+}
+
+function sum(x,y) {
+    console.log(x+y)
+}
+
+plus(111)
+display("qwert")
+fullname("naveeharn","marker")
+fullname("marker")
+sum(112,44)
