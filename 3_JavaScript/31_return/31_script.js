215b9edbb43340ed701df8116a9168bf1a262101
naveeharn <naveeharn@hotmail.com>
2022-02-22 21:32:33 +0700

    first commit

---
+++ 3_JavaScript/31_return/31_script.js
@@ -0,0 +1,12 @@
+function getComputerIP() {
+    return "127.0.0.1"
+}
+
+function getFactorial() {
+    return 1*2*3*4*5
+}
+
+// getComputerIP()
+console.log(getComputerIP())
+
+console.log(getFactorial())
