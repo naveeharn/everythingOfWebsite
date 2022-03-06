0acad46beedd37b22643a7f356a1a0eca881c528
naveeharn <naveeharn@hotmail.com>
2022-03-06 22:51:07 +0700

    first commit

---
+++ 3_JavaScript/36_forLoop/36_script.js
@@ -0,0 +1,7 @@
+let colors = ["red","white","blue","purple"]
+
+for (let index = 0; index < colors.length; index++) {
+    // const element = colors[index];
+    console.log("index",index,colors[index])
+    
+}
