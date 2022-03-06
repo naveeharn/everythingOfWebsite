908acdc6120a889c3aa2b2e918d385c55d96e4b5
naveeharn <naveeharn@hotmail.com>
2022-03-06 22:52:19 +0700

    first commit

---
+++ 3_JavaScript/39_concat/39_script.js
@@ -0,0 +1,9 @@
+let divide_2 = [2, 4, 6, 8, 10]
+let divide_3 = [3, 6, 9, 12, 15]
+let divide_4 = [4, 8, 12, 16, 20]
+
+let merge = divide_2.concat(divide_3,divide_4)
+
+console.log(merge)
+console.log(merge.sort())
+console.log(merge.reverse())
