f6cfe590675fcbc3a793d8bd99dea234dcea2aa0
naveeharn <naveeharn@hotmail.com>
2022-03-06 22:51:46 +0700

    first commit

---
+++ 3_JavaScript/38_converterArray/38_script.js
@@ -0,0 +1,8 @@
+let colors = ["red","white","blue","purple"]
+let toString = colors.toString()
+
+console.log(typeof(colors))
+console.log(toString)
+
+let join = colors.join("-kuy-")
+console.log(join)
