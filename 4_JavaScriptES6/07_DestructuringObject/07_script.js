bf4f8fd7a65c20ae782f92a9a5ed11040b9d4283
naveeharn <naveeharn@hotmail.com>
2022-03-09 20:32:31 +0700

    first commit

---
+++ 4_JavaScriptES6/07_DestructuringObject/07_script.js
@@ -0,0 +1,28 @@
+
+var person = {
+    name: 'Joey',
+    skill: 'C++, Java, Python',
+}
+
+var name = person.name
+var skill = person.skill
+
+console.log(name, skill)
+console.log(`
+name : ${name}
+skill : ${skill} 
+`)
+
+
+
+var player = {
+    namePlayer: 'Kranuey',
+    skillPlayer: 'HTML, CSS, JavaScript',
+}
+
+let { namePlayer, skillPlayer } = player
+
+console.log(`
+name : ${namePlayer}
+skill : ${skillPlayer} 
+`)
