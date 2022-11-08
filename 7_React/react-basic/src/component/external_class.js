18a4f4c13226e56f118f3af0d85b146a0151c754
naveeharn <naveeharn@hotmail.com>
2022-11-08 22:18:13 +0700

    add file

---
+++ 7_React/react-basic/src/component/external_class.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+class ClassExternalComponent extends React.Component{
+    render(){
+        return <h1>
+            Class External Component
+        </h1>
+    }
+}
+
+export default ClassExternalComponent
