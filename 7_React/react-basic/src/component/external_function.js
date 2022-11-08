366d76f082b0ec957d7e243c2478b2d4ec6fb02a
naveeharn <naveeharn@hotmail.com>
2022-11-08 22:18:17 +0700

    add file

---
+++ 7_React/react-basic/src/component/external_function.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function FuncExternalComponent(params) {
+    return <h1>
+        Function External Component {params}
+    </h1>
+}
+export default FuncExternalComponent
